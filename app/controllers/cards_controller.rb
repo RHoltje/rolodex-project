89a402f40fe1c4b5c642e1b4f693a329dbf9e97d
Robin Höltje <labrat@gerf.org>
2019-11-12 11:57:22 -0500

    card_controller.rb and card show.html.erb lookiong good

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,11 +1,21 @@
 class CardsController < ApplicationController
+  def show
+    @card = Card.find(params[:id])
+  end
+
   def new
   end
 
   def create
-    @card = Card.new(params[:card])
+    @card = Card.new(cards_params)
 
     @card.save
     redirect_to @card
+  end
+
+  private
+
+  def cards_params
+    params.require(:card).permit(:first_Name, :last_Name, :address)
   end
 end
